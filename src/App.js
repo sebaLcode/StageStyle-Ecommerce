9f71d99c912cd54e815fca9d27e8261508e36c8c
sebaLcode <seba.lcode@gmail.com>
2025-11-03 06:00:26 +0000

    Se añadén las rutas respectivas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegistrationPage from './components/pages/RegistrationPage';
-import Home from './components/pages/Home';
 import './App.css';
+import Home from './components/pages/Home';
 import LoginPage from './components/pages/LoginPage';
 import ProductosPage from './components/pages/ProductosPage';
-
+import RegistrationPage from './components/pages/RegistrationPage';
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Home />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegistrationPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/productos" element={<ProductosPage />} />
