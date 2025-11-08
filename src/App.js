d25f817273a38a191db471e33960208c5691e944
sebaLcode <seba.lcode@gmail.com>
2025-11-08 23:10:20 +0000

    Se modifican las rutas de la página, limitando el acceso según tipo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Home from './components/pages/Home';
 import LoginPage from './components/pages/LoginPage';
 import ProductosPage from './components/pages/ProductosPage';
 import RegistrationPage from './components/pages/RegistrationPage';
-
+import AdminPage from './components/pages/AdminHome';
+import AdminInventory from './components/pages/AdminInventory';
+import AdminNewProduct from './components/pages/AdminNewProduct';
+import AdminCustomers from './components/pages/AdminCustomers';
+import AdminEmployees from './components/pages/AdminEmployees';
+import AdminNewUser from './components/pages/AdminNewUser';
+import ProtectedRoute from './components/routes/ProtectedRoutes';
 
 function App() {
   return (
@@ -15,6 +21,55 @@ function App() {
           <Route path="/register" element={<RegistrationPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/productos" element={<ProductosPage />} />
+
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute allowedRoles={['Administrador', 'Vendedor']}>
+                <AdminPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/inventory"
+            element={
+              <ProtectedRoute allowedRoles={['Administrador', 'Vendedor']}>
+                <AdminInventory />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/inventory/new"
+            element={
+              <ProtectedRoute allowedRoles={['Administrador', 'Vendedor']}>
+                <AdminNewProduct />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/customers"
+            element={
+              <ProtectedRoute allowedRoles={['Administrador', 'Vendedor']}>
+                <AdminCustomers />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/employees"
+            element={
+              <ProtectedRoute allowedRoles={['Administrador', 'Vendedor']}>
+                <AdminEmployees />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/newUser"
+            element={
+              <ProtectedRoute allowedRoles={['Administrador']}>
+                <AdminNewUser />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './styles/globals.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
